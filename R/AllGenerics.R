8e9844c41970edba8ad0119ba8c3a71d767e0885
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-17 09:35:28 +0000

    update NAMESPACE
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clusterProfiler@59335 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,8 +1,10 @@
 ##' @exportMethod show
 setGeneric("show", function(object) standardGeneric("show"))
 
+
 ##' @exportMethod summary
 setGeneric("summary", function(object) standardGeneric("summary"))
+
 
 ##' @exportMethod plot
 setGeneric("plot", function(x, ...) standardGeneric("plot"))
