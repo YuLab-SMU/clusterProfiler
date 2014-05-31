f8fdef36582348d0a9ea540d28822b0da4ebeb71
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-31 16:37:15 +0000

    setting up git-svn bridge
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clusterProfiler@90901 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-########### zzz.R
-.onLoad <- function(libname, pkgname) {
-#	pkgVersion <- packageDescription(pkgname, fields="Version")
-#	msg <- paste("\nWelcome to", pkgname, "version", pkgVersion, "\n")
-#	packageStartupMessage(msg)
-	.initial()
-}
