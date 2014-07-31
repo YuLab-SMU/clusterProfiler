7fc3fe2adb862fd56d3981b944156e2cee872e72
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-07-31 06:21:01 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 1ca9919d5f00d6bf1a4cc262f4283de5b07f97c0
    Commit message: add gsea, cnetplot, enrichMap
    Committed by: Guangchuang Yu
    Author Name: Guangchuang Yu
    Commit date: 2014-07-31 14:20:17 +0800
    Author date: 2014-07-31 14:20:17 +0800
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clusterProfiler@93075 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/enrichGO.R
+++ R/enrichGO.R
@@ -53,20 +53,50 @@ enrichGO <- function(gene,
 }
 
 
+##' enrichment map
+##'
+##' enrichMap
+##' @title enrichMap
+##' @param x enrichResult or gseaResult
+##' @param ... additional parameter
+##' @return figure
+##' @export
+##' @author ygc
+enrichMap <- function(x, ...) {
+    ## DOSE::enrichMap(...)
+    plot(x, type="enrichMap", ...)
+}
+
+##' category-gene-net plot
+##'
+##' category gene association
+##' @title cnetplot
+##' @param x enrichResult object
+##' @param ... additional parameter
+##' @return figure
+##' @export
+##' @author ygc
+cnetplot <- function(x, ...) {
+    plot(x, type="cnet", ...)
+}
+
 ##' @importFrom DOSE EXTID2TERMID
-##' @S3method EXTID2TERMID MF
+##' @method EXTID2TERMID MF
+##' @export
 EXTID2TERMID.MF <- function(gene, organism) {
     EXTID2TERMID.GO(gene=gene, ont="MF", organism=organism)
 }
 
 ##' @importFrom DOSE EXTID2TERMID
-##' @S3method EXTID2TERMID BP
+##' @method EXTID2TERMID BP
+##' @export
 EXTID2TERMID.BP <- function(gene, organism) {
     EXTID2TERMID.GO(gene=gene, ont="BP", organism=organism)
 }
 
 ##' @importFrom DOSE EXTID2TERMID
-##' @S3method EXTID2TERMID CC
+##' @method EXTID2TERMID CC
+##' @export
 EXTID2TERMID.CC <- function(gene, organism) {
     EXTID2TERMID.GO(gene=gene, ont="CC", organism=organism)
 }
@@ -135,19 +165,22 @@ EXTID2TERMID.GO <- function(gene, ont, organism) {
 }
 
 ##' @importFrom DOSE TERMID2EXTID
-##' @S3method TERMID2EXTID MF
+##' @method TERMID2EXTID MF
+##' @export
 TERMID2EXTID.MF <- function(term, organism) {
     TERMID2EXTID.GO(term, organism)
 }
 
 ##' @importFrom DOSE TERMID2EXTID
-##' @S3method TERMID2EXTID BP
+##' @method TERMID2EXTID BP
+##' @export
 TERMID2EXTID.BP <- function(term, organism) {
     TERMID2EXTID.GO(term, organism)
 }
 
 ##' @importFrom DOSE TERMID2EXTID
-##' @S3method TERMID2EXTID CC
+##' @method TERMID2EXTID CC
+##' @export
 TERMID2EXTID.CC <- function(term, organism) {
     TERMID2EXTID.GO(term, organism)
 }
@@ -157,11 +190,20 @@ TERMID2EXTID.CC <- function(term, organism) {
 TERMID2EXTID.GO <- function(term, organism) {
     term <- as.character(term)
 
+    GO2ALLEG <- GO2EXTID(organism)
+    if (is(GO2ALLEG, "Go3AnnDbBimap")) {
+        GO2ExtID <- mget(term, GO2ALLEG, ifnotfound=NA)
+        GO2ExtID <- lapply(GO2ExtID, function(i) unique(i))
+    } else {
+        GO2ExtID <- GO2ALLEG[term]
+    }
+    return(GO2ExtID)
+}
+
+GO2EXTID <- function(organism) {
     supported_Org <- getSupported_Org()
     if (organism %in% supported_Org) {
-        mappedDb <- getGO2ALLEG_MappedDb(organism)
-        GO2ExtID <- mget(term, mappedDb, ifnotfound=NA)
-        GO2ExtID <- lapply(GO2ExtID, function(i) unique(i))
+        GO2ALLEG <- getGO2ALLEG_MappedDb(organism)
     } else {
         oldwd <- getwd()
         if(organism == "D39") {
@@ -175,32 +217,34 @@ TERMID2EXTID.GO <- function(term, organism) {
         if (file.exists("GO2ALLEG.rda")) {
             GO2ALLEG <- NULL # to satisfy codetools
             load("GO2ALLEG.rda")
-            GO2ExtID <- GO2ALLEG[term]
         } else {
             setwd(oldwd)
             stop("GO Mapping file not found in the working directory")
         }
         setwd(oldwd)
     }
-    return(GO2ExtID)
+    return(GO2ALLEG)
 }
 
 
-
 ##' @importFrom DOSE ALLEXTID
-##' @S3method ALLEXTID MF
+##' @method ALLEXTID MF
+##' @export
 ALLEXTID.MF <- function(organism) {
     ALLEXTID.GO(organism)
 }
 
 ##' @importFrom DOSE ALLEXTID
-##' @S3method ALLEXTID BP
+##' @method ALLEXTID BP
+##' @export
 ALLEXTID.BP <- function(organism) {
     ALLEXTID.GO(organism)
 }
 
 ##' @importFrom DOSE ALLEXTID
-##' @S3method ALLEXTID CC
+## @S3method ALLEXTID CC
+##' @method ALLEXTID CC
+##' @export
 ALLEXTID.CC <- function(organism) {
     ALLEXTID.GO(organism)
 }
@@ -237,19 +281,22 @@ ALLEXTID.GO <- function(organism) {
 }
 
 ##' @importFrom DOSE TERM2NAME
-##' @S3method TERM2NAME MF
+##' @method TERM2NAME MF
+##' @export
 TERM2NAME.MF <- function(term, organism) {
     TERM2NAME.GO(term, organism)
 }
 
 ##' @importFrom DOSE TERM2NAME
-##' @S3method TERM2NAME BP
+##' @method TERM2NAME BP
+##' @export
 TERM2NAME.BP <- function(term, organism) {
     TERM2NAME.GO(term, organism)
 }
 
 ##' @importFrom DOSE TERM2NAME
-##' @S3method TERM2NAME CC
+##' @method TERM2NAME CC
+##' @export
 TERM2NAME.CC <- function(term, organism) {
     TERM2NAME.GO(term, organism)
 }
