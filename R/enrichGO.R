0846ddf94f8d7ecfcceb17c71a09228084c668b7
Guangchuang Yu <guangchuangyu@gmail.com>
2015-06-21 15:06:04 +0000

    import dotplot from DOSE
    From: GuangchuangYu <guangchuangyu@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clusterProfiler@105275 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/enrichGO.R
+++ R/enrichGO.R
@@ -56,35 +56,6 @@ enrichGO <- function(gene,
 }
 
 
-##' enrichment map
-##'
-##' enrichMap
-##' @title enrichMap
-##' @param x gseaResult or enrichResult object
-##' @param n maximum number of category to shown
-##' @param fixed if set to FALSE, will invoke tkplot
-##' @param vertex.label.font font size of vertex label
-##' @param ... additional parameter
-##' @return figure
-##' @export
-##' @author ygc
-enrichMap <- DOSE::enrichMap
-
-##' category-gene-net plot
-##'
-##' category gene association
-##' @title cnetplot
-##' @param x enrichResult object
-##' @param showCategory number of category plotted
-##' @param categorySize one of geneNum or pvalue
-##' @param foldChange fold change of expression value
-##' @param fixed logical
-##' @param ... additional parameter
-##' @return plot
-##' @export
-##' @author ygc
-cnetplot <- DOSE:::cnetplot.enrichResult
-
 ##' @importFrom DOSE EXTID2TERMID
 ##' @method EXTID2TERMID MF
 ##' @export
