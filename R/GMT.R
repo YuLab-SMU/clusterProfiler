71b9f38dec95e2181507d18cac130c5fdc6fcd7c
Guangchuang Yu <guangchuangyu@gmail.com>
2015-10-28 02:26:43 +0000

    read.gmt
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clusterProfiler@110018 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/GMT.R
@@ -0,0 +1,18 @@
+##' parse gmt file to a data.frame
+##'
+##' 
+##' @title read.gmt
+##' @param gmtfile gmt file
+##' @importFrom GSEABase getGmt
+##' @importFrom GSEABase geneIds
+##' @export
+##' @return data.frame 
+##' @author Guangchuang Yu
+read.gmt <- function(gmtfile) {
+    gmt <- getGmt(con=gmtfile)
+    ont2gene <- geneIds(gmt) %>% stack
+    ont2gene <- ont2gene[, c("ind", "values")]
+    colnames(ont2gene) <- c("ont", "gene")
+    return(ont2gene)
+}
+
