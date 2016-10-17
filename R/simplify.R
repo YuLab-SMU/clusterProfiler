e44ef9fc143a88e3daa36f5f46cee95dd78ce664
guangchuang yu <guangchuangyu@gamil.com>
2016-10-17 10:39:04 +0800

    bug fixed of simplify

--- R/simplify.R
+++ R/simplify.R
@@ -22,11 +22,11 @@ setMethod("simplify", signature(x="enrichResult"),
           function(x, cutoff=0.7, by="p.adjust", select_fun=min, measure="Wang", semData = NULL) {
               if (!x@ontology %in% c("BP", "MF", "CC"))
                   stop("simplify only applied to output from enrichGO...")
-              
-              
+
+
               x@result %<>% simplify_internal(., cutoff, by, select_fun,
                                               measure, x@ontology, semData)
-              
+
               return(x)
           }
           )
@@ -52,39 +52,40 @@ simplify_internal <- function(res, cutoff=0.7, by="p.adjust", select_fun=min, me
                   semData = semData,
                   measure=measure,
                   combine=NULL)
-    
+
     ## to satisfy codetools for calling gather
     go1 <- go2 <- similarity <- NULL
-    
+
     sim.df <- as.data.frame(sim)
     sim.df$go1 <- row.names(sim.df)
     sim.df <- gather(sim.df, go2, similarity, -go1)
-    
+
     sim.df <- sim.df[!is.na(sim.df$similarity),]
-    
+
     ## feature 'by' is attached to 'go1'
     sim.df <- merge(sim.df, res[, c("ID", by)], by.x="go1", by.y="ID")
     sim.df$go2 <- as.character(sim.df$go2)
-    
+
     ID <- res$ID
-    
+
     GO_to_remove <- character()
     for (i in seq_along(ID)) {
         ii <- which(sim.df$go2 == ID[i] & sim.df$similarity > cutoff)
         ## if length(ii) == 1, then go1 == go2
-        if (length(ii) < 2) 
+        if (length(ii) < 2)
             next
-        
+
         sim_subset <- sim.df[ii,]
-        
+
         jj <- which(sim_subset[, by] == select_fun(sim_subset[, by]))
-        
+
         ## sim.df <- sim.df[-ii[-jj]]
         GO_to_remove <- c(GO_to_remove, sim_subset$go1[-jj]) %>% unique
     }
-    
+
     res[!res$ID %in% GO_to_remove, ]
 }
+
 
 
 ##' simplify output from compareCluster by removing redundancy of enriched GO terms
@@ -100,18 +101,18 @@ simplify_internal <- function(res, cutoff=0.7, by="p.adjust", select_fun=min, me
 ##' @aliases simplify,compareClusterResult-method
 ##' @author Guangchuang Yu
 setMethod("simplify", signature(x="compareClusterResult"),
-          function(x, cutoff=0.7, by="p.adjust", select_fun=min, measure="Rel") {
+          function(x, cutoff=0.7, by="p.adjust", select_fun=min, measure="Rel", semData) {
               res <- x@compareClusterResult
               ont <- get_go_ontology(x)
-              
-              organism <- x@.call$organism
-              if (is.null(organism)) {
-                  organism <- "human"
-              }
+
+              ## organism <- x@.call$organism
+              ## if (is.null(organism)) {
+              ##     organism <- "human"
+              ## }
 
               ## to satisfy codetools in calling subset
               Cluster <- NULL
-              lres <- lapply(unique(res$Cluster), function(cls) subset(res, Cluster == cls)) 
+              lres <- lapply(unique(res$Cluster), function(cls) subset(res, Cluster == cls))
 
               lres %<>% lapply(., simplify_internal,
                                cutoff=cutoff,
@@ -119,8 +120,8 @@ setMethod("simplify", signature(x="compareClusterResult"),
                                select_fun = select_fun,
                                measure = measure,
                                ont = ont,
-                               organism = organism)
-              
+                               semData = semData)
+
               x@compareClusterResult <- do.call("rbind", lres)
               return(x)
           }
