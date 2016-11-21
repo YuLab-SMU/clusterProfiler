e55d5fc10d5374b5b86e38e0d468529e54d55298
guangchuang yu <guangchuangyu@gamil.com>
2016-11-21 10:06:55 +0800

    update example of enrichGO

--- R/clusterProfiler-package.R
+++ R/clusterProfiler-package.R
@@ -3,23 +3,9 @@
 #' The package implements methods to analyze and visualize functional profiles
 #' of gene and gene clusters.
 #'
-#' This package is designed to compare gene clusters functional profiles.
-#'
-#' \tabular{ll}{ Package: \tab clusterProfiler\cr Type: \tab Package\cr
-#' Version: \tab 1.9.\cr Date: \tab 06-13-2013\cr biocViews:\tab GO,
-#' Clustering, Visulization\cr Depends:\tab AnnotationDbi, GO.db, org.Hs.eg.db,
-#' ggplot2, plyr, methods\cr Suggests:\tab GOSemSim\cr License: \tab
-#' Artistic-2.0\cr }
-#'
 #' @name clusterProfiler-package
 #' @aliases clusterProfiler-package clusterProfiler
 #' @docType package
-#' @author Guangchuang Yu \email{guangchuangyu@@gmail.com}
-#'
-#' Maintainer: Guangchuang Yu \email{guangchuangyu@@gmail.com}
-#' @seealso \linkS4class{compareClusterResult}, \linkS4class{groupGOResult}
-#'   \linkS4class{enrichResult}
-#' @keywords package
 NULL
 
 
