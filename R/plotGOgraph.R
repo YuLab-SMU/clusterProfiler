ce6942141eba255f7c537d59d212018bd3fa9fc0
Guangchuang Yu <guangchuangyu@gmail.com>
2017-02-27 08:40:50 +0000

    better support of plotGOgraph for gseGO, use core enriched gene info <2017-02-27, Mon>
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clusterProfiler@126991 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotGOgraph.R
+++ R/plotGOgraph.R
@@ -1,10 +1,10 @@
 ##' plot GO graph
 ##'
-##' 
+##'
 ##' @title plotGOgraph
 ##' @param x output of enrichGO or gseGO
 ##' @param firstSigNodes number of significant nodes (retangle nodes in the graph)
-##' @param useInfo additional info 
+##' @param useInfo additional info
 ##' @param sigForAll if TRUE the score/p-value of all nodes in the DAG is shown, otherwise only score will be shown
 ##' @param useFullNames logical
 ##' @param ... additional parameter of showSigOfNodes, please refer to topGO
@@ -27,44 +27,46 @@ plotGOgraph <- function(x,
     groupGOTerms <- get_fun_from_pkg("topGO", "groupGOTerms")
     annFUN.gene2GO <- get_fun_from_pkg("topGO", "annFUN.gene2GO")
     showSigOfNodes <- get_fun_from_pkg("topGO", "showSigOfNodes")
-    
+
     if (! class(x) %in% c("gseaResult", "enrichResult")) {
         stop("x should be output of gseGO or enrichGO...")
     }
-    
+
     gs <- x@geneSets
     gs.df <- data.frame(gene = unlist(gs),
                         go   = rep(names(gs),
-                            times=sapply(gs, length)))
+                                   times=sapply(gs, length)))
     gene2GO <- split(as.character(gs.df$go), gs.df$gene)
 
     if (is(x, "gseaResult")) {
         ont <- x@setType
         allgenes <- x@geneList
-        selector <- function(scores) return(TRUE) 
+        core_genes <- unique(unlist(geneInCategory(x)))
+        allgenes[!names(allgenes) %in% core_genes] <- -1
+        allgenes[core_genes] <- 1
     } else {
         ont <- x@ontology
         universe <- x@universe
         allgenes <- numeric(length(universe))
         names(allgenes) <- universe
         allgenes[x@gene] <- 1
-
-        selector <- function(scores) return(scores == 1)
     }
+
+    selector <- function(scores) return(scores == 1)
 
     if ( ! ont %in% c("BP", "MF", "CC")) {
         stop("ontology should be one of 'BP', 'MF' or 'CC'...")
     }
-    
-    
+
+
     pvalue <- x@result$p.adjust
     names(pvalue) <- x@result$ID
 
     groupGOTerms()
-     
+
     GOdata <- new("topGOdata",
                   description="clusterProfiler enrichment results",
-                  ontology = x@ontology,
+                  ontology = ont,
                   allGenes = allgenes,
                   geneSel = selector,
                   annot = annFUN.gene2GO,
