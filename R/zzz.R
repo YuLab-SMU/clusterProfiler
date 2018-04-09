5f01873080b8bbc383692ac6dd1683b506582aa3
guangchuang yu <guangchuangyu@gamil.com>
2018-04-09 14:58:12 +0800

    typo

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
                   "For help: https://guangchuangyu.github.io/software/", pkgname, "\n\n")
 
     citation <- paste0("If you use ", pkgname, " in published research, please cite:\n",
-                       "Guangchuang Yu., Li-Gen Wang, Yanyan Han, Qing-Yu He. ",
+                       "Guangchuang Yu, Li-Gen Wang, Yanyan Han, Qing-Yu He. ",
                        "clusterProfiler: an R package for comparing biological themes among gene clusters. ",
                        "OMICS: A Journal of Integrative Biology. 2012, 16(5):284-287.")
 
