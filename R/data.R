4d1ac55d175805ab8876585c617d8afdf22186ce
mjchen1996 <mjchen1996@outloo.com>
2021-02-09 18:56:47 +0800

    Add KEGG enrichment analysis of Microbiome data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' A list with 6980 K genes of human gut genome
+#' 
+#' All 6980 K genes were reported on 
+#' "An integrated catalog of reference genes in the
+#'  human gut microbiome.[J]. Nature Biotechnology, 2014."
+#' 
+#' @format A list with 6980 K genes
+"hgmlist"
