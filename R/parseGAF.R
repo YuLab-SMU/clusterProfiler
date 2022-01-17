eda41679fd132aedf90ca5991e13f003d1454349
778055611 <778055611@qq.com>
2022-01-17 11:31:41 +0800

    some modifications for coding style ,such as space, function names, variable names and indentations.

--- R/parseGAF.R
+++ R/parseGAF.R
@@ -1,8 +1,8 @@
-##' parse GAF file
+##' parse GAF files
 ##'
 ##' given a GAF file, this function extracts the information from it 
-##' @title parse.GAF
-##' @param gafFile GAF file
+##' @title ParseGafFile
+##' @param GafFile GAF file
 ##' @param nrows   a parameter
 ##' @return a list with two dataframes
 ##' @export
@@ -13,50 +13,75 @@
 ##' @importFrom utils read.delim
 ##' @importFrom stats na.omit
 ##' @importFrom AnnotationDbi columns
-parse.GAF <- function(gafFile,nrows=-1){
-  gafFile <-read.GAF(gafFile)
-  extr.gafFile <- gafFile[,c( "DB_Object_ID","GOID")]
+ParseGafFile <- function(GafFile, nrows=-1) {
   
-  offspring.CC <- as.data.frame(GOCCOFFSPRING)
-  offspring.MF <- as.data.frame(GOMFOFFSPRING)
-  offspring.BP <- as.data.frame(GOBPOFFSPRING)
+    GafFile <- ReadGafFile(GafFile)
+    extr.gafFile <- GafFile[, c("DB_Object_ID", "GOID")]
   
-  offspring.total <- rbind(offspring.CC,offspring.MF,offspring.BP)
-  names(offspring.total) <- c("OFFID","AncestorID")
+    offspring.CC <- as.data.frame(GOCCOFFSPRING)
+    offspring.MF <- as.data.frame(GOMFOFFSPRING)
+    offspring.BP <- as.data.frame(GOBPOFFSPRING)
   
-  extr.offspring <- offspring.total[offspring.total$OFFID%in%extr.gafFile$GOID,]
+    offspring.total <- rbind(offspring.CC, offspring.MF, offspring.BP)
+    names(offspring.total) <- c("OFFID", "AncestorID")
   
-  mer.info <- merge(extr.offspring,extr.gafFile,by.x="OFFID",by.y="GOID",all=T)
-  mer.info <-na.omit(mer.info)
-  mer.info <- mer.info[!duplicated(mer.info),]
+    extr.offspring <-
+        offspring.total[offspring.total$OFFID %in% extr.gafFile$GOID, ]
   
-  need.info1 <- mer.info[,c(2,3)]
-  need.info2 <- mer.info[,c(1,3)]
+    mer.info <-
+        merge(
+            extr.offspring,
+            extr.gafFile,
+            by.x = "OFFID",
+            by.y = "GOID",
+            all = T
+            )
+    mer.info <- na.omit(mer.info)
+    mer.info <- mer.info[!duplicated(mer.info), ]
   
-  names(need.info1) <- c("GOID","DB_Object_ID")
-  names(need.info2) <- c("GOID","DB_Object_ID")
-  bind.info <- rbind(need.info1,need.info2)
+    need.info1 <- mer.info[, c(2, 3)]
+    need.info2 <- mer.info[, c(1, 3)]
   
-  end.info <- bind.info[!duplicated(bind.info),]
-  end.info <- end.info[order(end.info$GOID,end.info$DB_Object_ID),]
+    names(need.info1) <- c("GOID", "DB_Object_ID")
+    names(need.info2) <- c("GOID", "DB_Object_ID")
+    bind.info <- rbind(need.info1, need.info2)
   
-  go.ALL <- AnnotationDbi::select(GO.db, keys(GO.db), columns(GO.db))
-  need.anno <- go.ALL[go.ALL$GOID%in%unique(end.info$GOID),]
+    end.info <- bind.info[!duplicated(bind.info),]
+    end.info <- end.info[order(end.info$GOID, end.info$DB_Object_ID), ]
   
-  list(TERM2GENE = end.info[,c("GOID","DB_Object_ID")], 
-       TERM2NAME = need.anno[,c("GOID","TERM")])
+    go.ALL <- AnnotationDbi::select(GO.db, keys(GO.db), columns(GO.db))
+    need.anno <- go.ALL[go.ALL$GOID %in% unique(end.info$GOID), ]
+  
+    list(TERM2GENE = end.info[, c("GOID", "DB_Object_ID")],  
+         TERM2NAME = need.anno[, c("GOID", "TERM")])
   
 }
 
 ##' @importFrom utils read.delim
-read.GAF <- function(gafFile, nrows = -1) {
-  cat("Reading ", gafFile, ": ", sep="")
-  gafFile <- read.delim(gafFile, sep="\t", as.is=TRUE, quote="\"", fill=TRUE,
-                        header=FALSE, nrows=nrows,comment.char="!")
-  gafFile <- gafFile[,c(2,3,5,7,9,10)]
-  names(gafFile) <- c("DB_Object_ID", "DB_Object_Symbol", "GOID",
-                      "Evidence_Code","Aspect","DB_Object_Name")
-  cat("found", nrow(gafFile), "rows in this GAF file")
-  return(gafFile)
+ReadGafFile <- function(GafFile, nrows=-1) {
+  
+    cat("Reading ", GafFile, ": ", sep = "")
+    GafFile <-
+        read.delim(
+            GafFile,
+            sep = "\t",
+            as.is = TRUE,
+            quote = "\"",
+            fill = TRUE,
+            header = FALSE,
+            nrows = nrows,
+            comment.char = "!"
+            )
+    GafFile <- GafFile[, c(2, 3, 5, 7, 9, 10)]
+    names(GafFile) <- c(
+        "DB_Object_ID",
+        "DB_Object_Symbol",
+        "GOID",
+        "Evidence_Code",
+        "Aspect",
+        "DB_Object_Name"
+        )
+    cat("found", nrow(GafFile), "rows in this GAF file")
+    return(GafFile)
 }
 
