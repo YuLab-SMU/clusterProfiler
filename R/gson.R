9a8c62e4efae3b39e53604b7bab5932898d0007a
Chun-Hui Gao <gaospecial@gmail.com>
2022-07-27 21:09:26 +0800

    gson kegg mapper

--- R/gson.R
+++ R/gson.R
@@ -11,8 +11,7 @@ gson_KEGG <- function(species) {
     x <- download_KEGG(species)
     gsid2gene <- setNames(x[[1]], c("gsid", "gene"))
     gsid2name <- setNames(x[[2]], c("gsid", "name"))
-    y <- readLines("https://rest.kegg.jp/info/hsa")
-    version <- sub("\\w+\\s+", "", y[grep('Release', y)])
+    version <- kegg_release(species)
     gson(gsid2gene = gsid2gene, 
         gsid2name = gsid2name,
         species = species,
@@ -53,4 +52,142 @@ gson_GO <- function(OrgDb, keytype = 'ENTREZID', ont = "BP") {
         version = version,
         accessed_date = as.character(Sys.Date())
     )
+}
+
+
+#' Build KEGG annotation for novel species using KEGG Mapper
+#' KEGG Mapper service can annotate protein sequences for novel species with KO database, 
+#' and KO annotation need to be converted into Pathway or Module annotation, which
+#' can then be used in `clusterProfiler`
+#' 
+#' @export
+#' @return  a gson instance
+#' @param file the name of the file which comes from the KEGG Mapper service
+#' @param format string indicate format of KEGG Mapper result
+#' @param type string indicate annotation database
+#' @param species your species, NULL if ignored
+#' @param ... pass to gson::gson()
+#' @examples 
+#' \dontrun{
+#'  file = system.file('extdata', "kegg_mapper_blast.txt", package='clusterProfiler')
+#'  gson_KEGG_mapper(file, format = "BLAST", type = "pathway")
+#' }
+gson_KEGG_mapper = function(file, 
+                       format = c("BLAST","Ghost","Kofam"), 
+                       type = c("pathway","module"),
+                       species = NULL,
+                       ...){
+  message("Please be aware that this process need a active internet connection and may take a while to finish.")
+  format = match.arg(format)
+  type = match.arg(type)
+  if (format %in% c("BLAST", "Ghost")){
+    protein2ko = utils::read.delim(file, header = FALSE, col.names = c("id","ko")) %>% 
+      dplyr::filter(.data[["ko"]] != "") %>%
+      dplyr::mutate_all("as.character")
+  } else if (format == "Kofam"){
+    protein2ko = utils::read.delim(file, comment.char = "#", header = FALSE) %>% 
+      tidyr::separate(1, into = c("star", 'id', 'ko'), sep = " +", extra = "drop") %>% 
+      dplyr::select(-1) %>%
+      tidyr::drop_na() %>%
+      dplyr::mutate_all("as.character")
+  } else {
+    simpleError("Please specify a valid file format for your KEGG Mapper result.")
+  }
+  
+  if (type == "pathway"){
+    all_pathway = kegg_list("pathway")
+    colnames(all_pathway) = c("pathway","pathway_name")
+    all_pathway %<>%
+      dplyr::mutate_at(.vars = "pathway", .funs = "remove_db_prefix")
+    
+    ko2pathway = kegg_link("pathway", "ko")
+    colnames(ko2pathway) = c("ko", "pathway")
+    ko2pathway %<>%
+      dplyr::mutate_all(.funs = "remove_db_prefix") %>%
+      # remove redundant ko prefix pathway ids, only keep those like map00010
+      dplyr::filter(stringr::str_starts(.data[["pathway"]], "map"))
+    
+    # some KOs don't have pathway mapping, in which case NA will be induced at pathway
+    gsid2gene = protein2ko %>%
+      dplyr::left_join(ko2pathway, by = "ko") %>%
+      dplyr::select(c("pathway","id"))
+    na_id = gsid2gene %>% 
+      dplyr::filter(is.na(.data[["pathway"]])) %>%
+      dplyr::pull(.data[["id"]])
+    if (length(na_id) > 0){
+      warning(sprintf("%d lines of KEGG mapper result didn't have pathway map, and they are ignored.\n  Totally affected %d unique gene ids.\n  The first five id are:\n%s.",
+                      length(na_id), 
+                      dplyr::n_distinct(na_id), 
+                      paste0("    ", na_id[1:5], collapse = "\n")))
+      gsid2gene = gsid2gene %>%
+        dplyr::filter(!is.na(.data[["pathway"]]))
+    }
+    
+    gsid2name = gsid2gene %>%
+      dplyr::distinct(.data[["pathway"]]) %>%
+      dplyr::left_join(all_pathway, by = "pathway")
+  } else if (type == "module"){
+    # all kegg module
+    all_module = kegg_list("module")
+    colnames(all_module) = c("module","module_name")
+    all_module = all_module %>%
+      dplyr::mutate_at(.vars = "module", .funs = "remove_db_prefix")
+    
+    # ko2module
+    ko2module = kegg_link("module", "ko")
+    colnames(ko2module) = c( "ko", "module")
+    ko2module = ko2module %>%
+      dplyr::mutate_all(.funs = "remove_db_prefix")
+    
+    # module to gene
+    gsid2gene = protein2ko %>%
+      dplyr::left_join(ko2module, by = "ko") %>%
+      dplyr::select(c("module", "id"))
+    
+    # similar to pathway, some KOs don't mapp to any module
+    na_id = gsid2gene %>% 
+      dplyr::filter(is.na(.data[["module"]])) %>%
+      dplyr::pull(.data[["id"]])
+    if (length(na_id) > 0){
+      warning(sprintf("%d lines of KEGG mapper result didn't belong to any module, and they are ignored.\n  Totally affected %d unique gene ids.\n  The first five id are:\n%s.",
+                      length(na_id), 
+                      dplyr::n_distinct(na_id), 
+                      paste0("    ", na_id[1:5], collapse = "\n")))
+      gsid2gene = gsid2gene %>%
+        dplyr::filter(!is.na(.data[["module"]]))
+      
+    }
+    
+    # gsid2name
+    gsid2name = gsid2gene %>%
+      dplyr::distinct(.data[["module"]]) %>%
+      dplyr::left_join(all_module, by = "module")
+  } else {
+    simpleError('Please specify your target database (currently "pathway" or "module").')
+  }
+  
+  # construct gson object
+  release = kegg_release(type)
+  colnames(gsid2gene) = c("gsid", "gene")
+  colnames(gsid2name) = c("gsid", "name")
+  gson::gson(
+    gsid2gene = gsid2gene,
+    gsid2name = gsid2name,
+    species = species,
+    gsname = paste0("KEGG_", type),
+    version = release,
+    accessed_date = as.character(Sys.Date()),
+    ...
+  )
+}
+
+remove_db_prefix = function(x){
+  gsub("^[a-z]+:", "", x)
+}
+
+kegg_release = function(db){
+  url = paste0("https://rest.kegg.jp/info/", db)
+  y = readLines(url)
+  release <- sub("\\w+\\s+", "", y[grep('Release', y)])
+  return(release)
 }
