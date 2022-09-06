4c6648191794f142e08d6ca4a23b3fe105f99caa
Chun-Hui Gao <gaospecial@gmail.com>
2022-09-06 11:59:55 +0800

    Update enrichMKEGG.R

--- R/enrichMKEGG.R
+++ R/enrichMKEGG.R
@@ -16,8 +16,18 @@ enrichMKEGG <- function(gene,
                         maxGSSize = 500,
                         qvalueCutoff = 0.2) {
 
+  if (inherits(organism, "character")) {
     species <- organismMapper(organism)
     KEGG_DATA <- prepare_KEGG(species, "MKEGG", keyType)
+  }
+  else if (inherits(organism, "GSON")) {
+    KEGG_DATA <- organism
+    species <- KEGG_DATA@species
+  }
+  else {
+    stop("organism should be a species name or a GSON object")
+  }
+  
     res <- enricher_internal(gene,
                              pvalueCutoff  = pvalueCutoff,
                              pAdjustMethod = pAdjustMethod,
