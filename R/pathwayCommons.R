9053e53103600802735a17869fadccb597dc2cba
Jigyasa Gupta <75614134+Jigyasa-G@users.noreply.github.com>
2023-06-13 04:20:20 +0530

    Update ORA & GSEA methods

--- R/pathwayCommons.R
+++ R/pathwayCommons.R
@@ -22,3 +22,30 @@ enrichPC <- function(gene, organism, ...) {
 
     return(res)
 }
+
+##' GSEA analysis for  Pathway Commons
+##'
+##' This function performs GSEA using  Pathway Commons
+##' @title gsePC
+##' @param geneList ranked gene list
+##' @param organism supported organisms, which can be accessed via the get_pc_organisms() function
+##' @param ... additional parameters, see also the parameters supported by the GSEA() function
+##' @return A \code{gseaResult} instance
+##' @export
+##' @author Guangchuang Yu 
+gsePC <- function(geneList, organism, ...) {
+    pcdata <- prepare_PC_data(organism)
+    res <- GSEA(geneList,
+                TERM2GENE = pcdata$PCID2GENE,
+                TERM2NAME = pcdata$PCID2NAME,
+                ...)
+
+    if (is.null(res)) return(res)
+
+    res@setType <- "Pathway Commons"
+    res@organism <- organism
+    res@keytype <-  "ENTREZID"
+
+    return(res)
+}
+
