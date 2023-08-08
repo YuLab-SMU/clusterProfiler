22251301e2c5566d2711b5eda72b0b74195b82ae
Jigyasa Gupta <75614134+Jigyasa-G@users.noreply.github.com>
2023-08-09 01:36:43 +0530

    Update ssgsea.R

--- R/ssgsea.R
+++ R/ssgsea.R
@@ -1,6 +1,14 @@
-# This function performs ssGSEA Analysis
-
-
+##' Single Sample Gene Set Enrichment Analysis (ssGSEA)
+##'
+##' This function performs single-sample gene set enrichment analysis (ssGSEA)
+##'
+##' @title ssgsea
+##' @param gene_expression a matrix of gene expression data with genes in rows and samples in columns
+##' @param genes a vector of gene identifiers
+##' @param num_gene_sets the number of gene sets to generate and use for ssGSEA (default: 100)
+##' @param genes_per_set the number of genes to include in each generated gene set (default: 10)
+##' @return A matrix of ssGSEA enrichment scores with gene sets as rows and samples as columns
+##' @export
 
 ssgsea <- function(gene_expression, genes, num_gene_sets=100, genes_per_set=10) {
   gs <- list()
@@ -13,5 +21,3 @@ ssgsea <- function(gene_expression, genes, num_gene_sets=100, genes_per_set=10) 
   ssGSEA_scores <- gsva(gene_expression, gs, method = "ssgsea")
   return(ssGSEA_scores)
 }
-
-# ssgsea(gene_expression, genes, num_gene_sets, genes_per_set)
