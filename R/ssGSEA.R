6fd974a36ffa73ed6507ea12a7733aa7ffe3d895
Jigyasa Gupta <75614134+Jigyasa-G@users.noreply.github.com>
2023-08-09 01:21:38 +0530

    Method creation for ssGSEA

--- R/ssGSEA.R
+++ R/ssGSEA.R
@@ -1,1 +1,15 @@
 #Perform ssGSEA Analysis
+
+ssgsea <- function(gene_expression, genes, num_gene_sets, genes_per_set) {
+  gs <- list()
+  for (i in 1:num_gene_sets) {
+    sampled_genes <- sample(genes, genes_per_set, replace = FALSE)
+    gs[[i]] <- sampled_genes
+  }
+  names(gs) <- paste0("gs", 1:length(gs))
+
+  ssGSEA_scores <- gsva(gene_expression, gs, method = "ssgsea")
+  return(ssGSEA_scores)
+}
+
+# ssgsea(gene_expression, genes, num_gene_sets, genes_per_set)
